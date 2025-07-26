6bb045c03f6855e92b99412fc6fcbb629083746d
Aswin Babu <161277857+Aswinbabu6233@users.noreply.github.com>
2025-07-26 12:48:40 +0530

    Update BaseUrl.jsx

--- Forntend/src/utils/BaseUrl.jsx
+++ Forntend/src/utils/BaseUrl.jsx
@@ -1,4 +1,4 @@
-const mainURL = "https://jobnest-backend-wld8.onrender.com";
+const mainURL = "https://job-portal-backend-nbgz.onrender.com";
 const BASE_API = `${mainURL}/api`;
 
 export default BASE_API;
